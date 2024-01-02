193e1918c70755eafd4afe3d953e61e3ef2636b5
vigneshbunty <vigneshbunty07@gmail.com>
2024-01-02 09:23:56 +0530

    Intial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request
+app=Flask(_name_)
+@app.route("/")
+def home_page(name=None):
+    return render_template('index.html',name=name)
+if _name=='main_':
+    app.run(host='0.0.0.0',port=5000)
